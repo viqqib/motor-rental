3e242daff92f40fb4465c5dda1de66215ca830ac
viqqib <faqihzaky43@gmail.com>
2024-12-28 12:37:43 +0700

    fixing css on hosted domain

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Three J Rental Motor')</title>
     @vite('resources/css/app.css')
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}"> <!-- Your compiled CSS -->
+    {{-- <link rel="stylesheet" href="{{ mix('css/app.css') }}"> <!-- Your compiled CSS --> --}}
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
