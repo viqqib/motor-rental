663fcb666456277efea9bf527a60cc9bada5ab09
viqqib <faqihzaky43@gmail.com>
2025-01-05 23:10:10 +0700

    styling the dashboard

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Motor;
+use App\Models\Rental;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class AdminController extends Controller
 {
@@ -14,11 +16,24 @@ class AdminController extends Controller
   
     public function index()
     {
-        // Admin dashboard logic
+
+        $today = Carbon::today(); // Start of today
+        $startOfWeek = Carbon::now()->startOfWeek(Carbon::MONDAY); // Start of the current week (Monday)
+        $endOfWeek = Carbon::now()->endOfWeek(Carbon::SUNDAY); // End of the current week (Sunday)
+        $startOfMonth = Carbon::now()->startOfMonth(); // Start of the current month
+        $endOfMonth = Carbon::now()->endOfMonth();
+
+        // Get counts
+        $rentedToday = Rental::whereDate('tgl_mulai', $today)->count();
+        $rentedThisWeek = Rental::whereBetween('tgl_mulai', [$startOfWeek, $endOfWeek])->count();
+        $rentedThisMonth = Rental::whereBetween('tgl_mulai', [$startOfMonth, $endOfMonth])->count();
+
+            // Admin dashboard logic
         $availableMotors = Motor::where('status', 'tersedia')->get();
-        $rentedMotors = Motor::where('status', 'tidak tersedia')->get();
+        $bookedMotors = Rental::where('status', 'dipesan')->get();
+        $rentedMotors = Rental::where('status', 'dirental')->get();
         $maintenancedMotors = Motor::where('status', 'perawatan')->get();
-        return view('admin.dashboard', compact('availableMotors', 'rentedMotors', 'maintenancedMotors'));
+        return view('admin.dashboard', compact('availableMotors', 'rentedMotors', 'maintenancedMotors', 'rentedToday', 'rentedThisWeek', 'rentedThisMonth', 'bookedMotors'));
     }
 
     /**
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,63 +3,127 @@
 @section('title', 'Dashboard')
 
 @section('content')
+<div class="bg-white rounded-md p-4">
+    <p class="text-xl mb-1 font-semibold text-teal-700">Jumlah motor yang disewa</p>
 
-
-    <div class="bg-white flex p-4 rounded-md gap-x-5 gap-y-10 flex-col md:flex-row">
-       
-        <div class=" py-2 px-5 rounded-md border border-gray-300 ">
-            <p class="text-xl font-semibold text-green-600">Motor Sedang Dirental</p>
-            <hr class="my-2">
-            <ul>
-                @foreach ($rentedMotors as $motor )
-                <li class="flex my-1 text-left items-center gap-x-2 py-1">
-                    <span class="rounded-md text-gray-600 font-bold">{{ $motor->nomor_plat }}</span>
-                    <p class="">                    
-                        {{ $motor->merek }} {{ $motor->tipe }}</p>
-                </li>
-                @endforeach
-            </ul>
+    <div class="flex gap-x-3 mb-4">
+        <div class="bg-teal-600 py-2 w-[170px] px-4 rounded-sm text-white">
+            <p class="font-semibold">Hari Ini</p>
+            <hr class="my-1">
+            <div>
+                <p class="text-7xl font-bold">{{ $rentedToday }}</p>
+                <p>Unit</p>
+            </div>
         </div>
 
-        <div class=" py-2 px-5 rounded-md border border-gray-300">
-            <p class="text-xl font-semibold text-red-400">Motor dalam perawatan</p>
-            <hr class="my-2">
-            <ul>
-                @if ($maintenancedMotors->isEmpty())
-                <h1>Tidak ada motor dalam perawatan</h1>
+        <div class="bg-blue-500 py-2 w-[170px] px-4 rounded-sm text-white">
+            <p class="font-semibold">Minggu Ini</p>
+            <hr class="my-1">
+            <div>
+                <p class="text-7xl font-bold">{{ $rentedThisWeek }}</p>
+                <p>Unit</p>
+            </div>
+        </div>
+
+        <div class="bg-red-400 py-2 w-[170px] px-4 rounded-sm text-white">
+            <p class="font-semibold">Bulan Ini</p>
+            <hr class="my-1">
+            <div>
+                <p class="text-7xl font-bold">{{ $rentedThisMonth }}</p>
+                <p>Unit</p>
+            </div>
+        </div>
+
+       
+    </div>
+
+    <!-- Motor Sedang Dirental -->
+    <p class="text-xl mb-1 font-semibold text-teal-600">Motor dalam penyewaan</p>
+        <div>
+            <table class="w-full text-left ">
+                <thead class="bg-teal-500 text-white rounded-t-lg">
+                    <tr>
+                        <th class="px-4 py-3 font-medium">Unit</th>
+                        <th class="px-4 py-3 font-medium">Penyewa</th>
+                        <th class="px-4 py-3 font-medium">Durasi Sewa</th>
+                        <th class="px-4 py-3 font-medium">Berakhir</th>
+                    </tr>
+                </thead>
+                <tbody class="divide-y divide-gray-100">
+                    @foreach ($rentedMotors as $motor)
+                        <tr class="hover:bg-gray-200 bg-gray-100">
+                            <td class="px-4 py-3 font-semibold text-gray-700">{{ $motor->motor->nomor_plat }} <span class="font-normal"> {{ $motor->motor->merek }} {{ $motor->motor->tipe }}</span></td>
+                            <td class="px-4 py-3 text-gray-600">{{ $motor->renter->name }}</td>
+                            <td class="px-4 py-3 text-gray-600">{{ $motor->durasi_sewa }} Hari</td>
+                            <td class="px-4 py-3 text-gray-600">
+                                {{ \Carbon\Carbon::parse($motor->tgl_selesai)->translatedFormat('j F Y') }}
+                            </td>
+                            
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+
+
+        <p class="text-xl mb-1 font-semibold text-teal-600 mt-5">Motor yang telah dibooking</p>
+        <div>
+            <table class="w-full text-left ">
+                <thead class="bg-teal-500 text-white rounded-t-lg">
+                    <tr>
+                        <th class="px-4 py-3 font-medium">Unit</th>
+                        <th class="px-4 py-3 font-medium">Penyewa</th>
+                        <th class="px-4 py-3 font-medium">Durasi Sewa</th>
+                        <th class="px-4 py-3 font-medium">Mulai</th>
+                    </tr>
+                </thead>
+                <tbody class="divide-y divide-gray-100">
+                    @foreach ($bookedMotors as $motor)
+                        <tr class="hover:bg-gray-200 bg-gray-100">
+                            <td class="px-4 py-3 font-semibold text-gray-700">{{ $motor->motor->nomor_plat }} <span class="font-normal"> {{ $motor->motor->merek }} {{ $motor->motor->tipe }}</span></td>
+                            <td class="px-4 py-3 text-gray-600">{{ $motor->renter->name }}</td>
+                            <td class="px-4 py-3 text-gray-600">{{ $motor->durasi_sewa }} Hari</td>
+                            <td class="px-4 py-3 text-gray-600">
+                                {{ \Carbon\Carbon::parse($motor->tgl_mulai)->translatedFormat('j F Y') }}
+                            </td>
+                            
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+
+    
+    <!-- Motor dalam perawatan -->
+    {{-- <div class="py-2 px-5 rounded-md mb-4 shadow-md border">
+        <p class="font-semibold text-red-400">Motor dalam Perawatan</p>
+        <hr class="my-2">
+        <ul class="list-disc">
+            @if ($maintenancedMotors->isEmpty())
+                <li class="text-gray-600">Tidak ada motor dalam perawatan</li>
             @else
                 @foreach ($maintenancedMotors as $motor)
-                    <li class="flex my-1 text-left items-center gap-x-2 py-1">
+                    <li class="flex items-center gap-x-2 py-1">
                         <span class="rounded-md text-gray-600 font-bold">{{ $motor->nomor_plat }}</span>
-                        <p class="">                    
-                            {{ $motor->merek }} {{ $motor->tipe }}
-                        </p>
+                        <p>{{ $motor->merek }} {{ $motor->tipe }}</p>
                     </li>
                 @endforeach
             @endif
-            </ul>
-        </div>
-
-        <div class=" py-2 px-5 rounded-md border border-gray-300">
-            <p class="text-xl font-semibold text-red-400">Motor yang tersedia</p>
-            <hr class="my-2">
-            <ul>
-                @foreach ($availableMotors as $motor )
-                <li class="flex my-1 text-left items-center gap-x-2 py-1">
-                    <span class="rounded-md text-gray-600 font-bold">{{ $motor->nomor_plat }}</span>
-                    <p class="">                    
-                        {{ $motor->merek }} {{ $motor->tipe }}</p>
-                </li>
-                @endforeach
-            </ul>
-        </div>
-
+        </ul>
     </div>
 
-    
-    {{-- <a href="/admin/motor" class="bg-teal-800 text-white py-2 px-5 rounded-md font-bold">Ke Motor</a>
-    <a href="/admin/motorHarga" class="bg-teal-800 text-white py-2 px-5 rounded-md font-bold">Ke Harga</a> --}}
-
-  
-    
+    <!-- Motor yang tersedia -->
+    <div class="py-2 px-5 rounded-md shadow-md border">
+        <p class="font-semibold text-teal-700">Motor yang Tersedia</p>
+        <hr class="my-2">
+        <ul class="list-disc">
+            @foreach ($availableMotors as $motor)
+                <li class="flex items-center gap-x-2 py-1">
+                    <span class="rounded-md text-gray-600 font-bold">{{ $motor->nomor_plat }}</span>
+                    <p>{{ $motor->merek }} {{ $motor->tipe }}</p>
+                </li>
+            @endforeach
+        </ul>
+    </div> --}}
+</div>
 @endsection
